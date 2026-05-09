be3e059834a4320958d5c8776d1234e6467caa62
林彥彣 <thomas@Thomass-MacBook-Pro.local>
2026-05-10 02:26:59 +0800

    connect frontend to notion api

--- api/save.js
+++ api/save.js
@@ -1,10 +1,11 @@
 export default async function handler(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "https://thom436.github.io");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Max-Age", "86400");
 
   if (req.method === "OPTIONS") {
-    return res.status(200).end();
+    return res.status(204).end();
   }
 
   if (req.method !== "POST") {
